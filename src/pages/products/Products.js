436f2f94b413e35d6695fabad8943b40e9148a08
Silvery86 <giang8692@gmail.com>
2023-04-29 18:42:47 +0700

    add search box to products page

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useLoaderData } from 'react-router-dom'
 
 export default function Products() {
-    const products = useLoaderData();
+  const [products, setProducts] = useState([])
+  const fetchData = useLoaderData();
+  useEffect(() => {
+    setProducts(fetchData)
+  },[fetchData])
+  
   return (
     <div className='products row'>
+        <input className="search-box col l-12 m-12 c-12" type="search" placeholder="Search product" onChange={(event) => {
+            const searchValue = event.target.value.toLocaleLowerCase();
+            const filterProducts = products.filter((product) => {
+                return product.productname.toLocaleLowerCase().includes(searchValue);
+            })
+            setProducts(filterProducts)
+            
+
+        }}/>
         {products.map(product => (
             <Link className="col l-3 m-6 c-6" to={product.id.toString()} key={product.id}>
             <p>{product.productname}</p>
@@ -16,6 +30,7 @@ export default function Products() {
     </div>
   )
 }
+
 
 export const productsLoader = async () => {
     const res = await fetch('http://localhost:4000/products') // api json data link
